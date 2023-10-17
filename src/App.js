e524f0c8a3966a4c575289643cedf0600a2bfe37
Raphaël <simonraphael@hotmail.fr>
2023-10-17 16:36:32 +0200

    reorganise-files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import WeatherApp from './components/WeatherApp';
+import SearchBar from './components/SearchBar';
+import City from './components/City.js';
+import WeatherLeft from './components/WeatherLeft';
+import WeatherRight from './components/WeatherRight';
 
 function App() {
   return (
-    <div className="App">
-      <WeatherApp />
-    </div>
+    <div className="container">
+          <SearchBar />
+          <City />
+          <div className="weather">
+              <WeatherLeft />
+              <WeatherRight />
+          </div>
+      </div>
   );
 }
 
